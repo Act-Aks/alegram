ebc87e166f60e13d32c34e407ac3039160915928
Act-Aks <akashsrivastava.git@gmail.com>
2024-05-17 19:27:28 +0530

    feat: first dev eas build

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,9 +3,23 @@ import ThemeProvider from '@/providers/ThemeProvider'
 
 import { Slot } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+import { useEffect } from 'react'
+import { PermissionsAndroid, Platform } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 export default function RootLayout() {
+  useEffect(() => {
+    const run = async () => {
+      if (Platform.OS === 'android') {
+        await PermissionsAndroid.requestMultiple([
+          'android.permission.POST_NOTIFICATIONS',
+          'android.permission.BLUETOOTH_CONNECT',
+        ])
+      }
+    }
+    run()
+  }, [])
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <ThemeProvider>
