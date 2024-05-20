d7383eba37ae86b7c8082c9ba68398121e41f9eb
Act-Aks <akashsrivastava.git@gmail.com>
2024-05-20 18:08:33 +0530

    feat: adds lint action

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
 import pluginJs from '@eslint/js'
-import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
 import globals from 'globals'
 
 export default [
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
-  pluginReactConfig,
+  {
+    ignores: ['.expo/**', 'babel.config.js', '.yarn'],
+  },
 ]
